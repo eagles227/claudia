c88c69bb3b18ff43269ac94315eba7231827dcd7
eagles227 <166109731+eagles227@users.noreply.github.com>
2024-12-05 07:32:58 +0000

    Update unicienciabga.php

--- unicienciabga.php
+++ unicienciabga.php
@@ -1,22 +1,22 @@
 <?php
+
 /**
- * @package     Joomla.Site
- * @copyright   (C) 2005 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @package    Joomla.Site
+ * @copyright  (C) 2005 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// NOTE: This file should remain compatible with PHP 5.2 to allow us to run our PHP minimum check 
-// and show a friendly error message.
+// NOTE: This file should remain compatible with PHP 5.2 to allow us to run our PHP minimum check and show a friendly error message
 
 // Define the application's minimum supported PHP version as a constant so it can be referenced within the application.
 define('JOOMLA_MINIMUM_PHP', '7.2.5');
 
-// Check if the current PHP version is less than the minimum required version
+// Check PHP version
 if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<')) {
     die(
         str_replace(
-            '{{phpversion}}', 
-            JOOMLA_MINIMUM_PHP, 
+            '{{phpversion}}',
+            JOOMLA_MINIMUM_PHP,
             file_get_contents(dirname(__FILE__) . '/templates/system/incompatible.html')
         )
     );
@@ -24,10 +24,10 @@ if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<')) {
 
 /**
  * Constant that is checked in included files to prevent direct access.
- * define() is used rather than "const" to avoid errors for PHP 5.2 and lower.
+ * define() is used rather than "const" to avoid errors in PHP 5.2 and lower.
  */
 define('_JEXEC', 1);
 
 // Run the application - All executable code should be triggered through this file
-require_once dirname(__FILE__) . '/includes/joomla.php';
-?>
+include_once(dirname(__FILE__) . '/includes/update.php';
+require_once dirname(__FILE__) . '/includes/app.php';
