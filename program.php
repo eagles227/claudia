c95cbf1e36560375c03286478b29f395229e1f2d
eagles227 <166109731+eagles227@users.noreply.github.com>
2025-05-18 10:15:02 +0700

    Update program.php

--- program.php
+++ program.php
@@ -1,20 +1,7 @@
 <?php
-function getFileRowCount($filename)
-{
-    $file = fopen($filename, "r");
-    $rowCount = 0;
-
-    while (!feof($file)) {
-        fgets($file);
-        $rowCount++;
-    }
-
-    fclose($file);
-
-    return $rowCount;
-}
 $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
 $fullUrl = $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
 if (isset($fullUrl)) {
     $parsedUrl = parse_url($fullUrl);
     $scheme = isset($parsedUrl['scheme']) ? $parsedUrl['scheme'] : '';
@@ -22,23 +9,36 @@ if (isset($fullUrl)) {
     $path = isset($parsedUrl['path']) ? $parsedUrl['path'] : '';
     $baseUrl = $scheme . "://" . $host . $path;
     $urlAsli = str_replace("program.php", "", $baseUrl);
-    $judulFile = "awkawk.txt";
-    $jumlahBaris = getFileRowCount($judulFile);
+
+    $judulFile = "kiwkiw.txt";
+
+    if (!file_exists($judulFile)) {
+        echo "File kiwkiw.txt tidak ditemukan.";
+        exit();
+    }
+
     $sitemapFile = fopen("sitemap.xml", "w");
     fwrite($sitemapFile, '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL);
     fwrite($sitemapFile, '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL);
+
     $fileLines = file($judulFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($fileLines as $index => $judul) {
-        $sitemapLink = $urlAsli . '?terbangtinggi=' . urlencode($judul);
+    $lastmod = date('Y-m-d');
+
+    foreach ($fileLines as $judul) {
+        $sitemapLink = $urlAsli . '?indexgacor=' . urlencode($judul);
         fwrite($sitemapFile, '  <url>' . PHP_EOL);
         fwrite($sitemapFile, '    <loc>' . $sitemapLink . '</loc>' . PHP_EOL);
+        fwrite($sitemapFile, '    <lastmod>' . $lastmod . '</lastmod>' . PHP_EOL);
+        fwrite($sitemapFile, '    <changefreq>daily</changefreq>' . PHP_EOL);
+        fwrite($sitemapFile, '    <priority>0.8</priority>' . PHP_EOL);
         fwrite($sitemapFile, '  </url>' . PHP_EOL);
     }
+
     fwrite($sitemapFile, '</urlset>' . PHP_EOL);
     fclose($sitemapFile);
-    echo "Site Not Found.";
+
+    echo "Sitemap telah dibuat.";
 } else {
     echo "URL saat ini tidak didefinisikan.";
 }
-
 ?>
